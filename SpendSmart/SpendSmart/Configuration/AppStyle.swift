e6d3583a091033c20abedb2abcbe6af0c93d328e
dtrognn <dtrognn@gmail.com>
2024-04-01 17:22:05 +0700

    setup font, color, layout

---
+++ SpendSmart/SpendSmart/Configuration/AppStyle.swift
@@ -0,0 +1,16 @@
+//
+//  AppStyle.swift
+//  SpendSmart
+//
+//  Created by dtrognn on 01/04/2024.
+//
+
+import Foundation
+
+struct AppStyle {
+    static var font: SFont = AppFont()
+    static var theme: STheme = AppTheme()
+    static var layout: SLayout = AppLayout()
+
+    init() {}
+}
