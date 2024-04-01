843856f057ac94b4aa3e9499c25ee78bac1ade5c
dtrognn <dtrognn@gmail.com>
2024-04-01 17:11:58 +0700

    init project

---
+++ SpendSmart/SpendSmart/SpendSmartApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpendSmartApp.swift
+//  SpendSmart
+//
+//  Created by dtrognn on 01/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct SpendSmartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
