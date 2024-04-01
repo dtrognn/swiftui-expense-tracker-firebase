eb96ff491851312fc3c43dce6e98385264fbcda9
dtrognn <dtrognn@gmail.com>
2024-04-01 17:48:48 +0700

    config router

--- SpendSmart/SpendSmart/System/AppDelegate.swift
+++ SpendSmart/SpendSmart/System/AppDelegate.swift
@@ -7,15 +7,15 @@
 
 import UIKit
 
-//@UIApplicationMain
-//class AppDelegate: UIResponder, UIApplicationDelegate {
-//    var window: UIWindow?
-//
-//    static var shared: AppDelegate!
-//
-//    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-//        window = UIWindow(frame: UIScreen.main.bounds)
-//
-//        return true
-//    }
-//}
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    static var shared: AppDelegate!
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+
+        return true
+    }
+}
