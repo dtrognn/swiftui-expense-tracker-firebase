fd78b6188acf9636d90a20a9976b41073a701970
dtrognn <dtrognn@gmail.com>
2024-04-02 11:10:59 +0700

    edit tabRouterView

---
+++ SpendSmart/SpendSmart/Features/Home/HomeRouter.swift
@@ -0,0 +1,24 @@
+//
+//  HomeRouter.swift
+//  SpendSmart
+//
+//  Created by dtrognn on 02/04/2024.
+//
+
+import SwiftUI
+
+class HomeRouter: BaseRouter<HomeRouter.Screen> {
+    enum Screen: IScreen {}
+
+    override func getInstanceScreen(_ screen: Screen) -> AnyView {}
+}
+
+struct HomeRouterView: View {
+    @StateObject private var router = HomeRouter()
+
+    var body: some View {
+        SSNavigationStackView(navigationPath: router.navigationPath) {
+            HomeView()
+        }.environmentObject(router)
+    }
+}
