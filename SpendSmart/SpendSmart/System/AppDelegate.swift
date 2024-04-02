483c899fdcad7e5ef5397ece1f0f0e61c70f3f90
dtrognn <dtrognn@gmail.com>
2024-04-02 13:31:35 +0700

    firebase configure

--- SpendSmart/SpendSmart/System/AppDelegate.swift
+++ SpendSmart/SpendSmart/System/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import FirebaseCore
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,6 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
+
+        FirebaseApp.configure()
 
         return true
     }
