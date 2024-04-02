337155044a4bab244f0de90231ff2200daf596c4
dtrognn <dtrognn@gmail.com>
2024-04-02 10:55:51 +0700

    create SplashScreenView

---
+++ SpendSmart/SpendSmart/Features/Splash/SplashScreenView.swift
@@ -0,0 +1,18 @@
+//
+//  SplashScreenView.swift
+//  SpendSmart
+//
+//  Created by dtrognn on 02/04/2024.
+//
+
+import SwiftUI
+
+struct SplashScreenView: View {
+    var body: some View {
+        Text("SplashScreenView")
+    }
+}
+
+#Preview {
+    SplashScreenView()
+}
