cbb2e2cf66329bc3b86ac981acad4dadde24c988
dtrognn <dtrognn@gmail.com>
2024-04-02 11:32:31 +0700

    setup router

--- SpendSmart/SpendSmart/Features/Splash/SplashScreenView.swift
+++ SpendSmart/SpendSmart/Features/Splash/SplashScreenView.swift
@@ -8,8 +8,15 @@
 import SwiftUI
 
 struct SplashScreenView: View {
+    @EnvironmentObject private var router: AppRouter
+
     var body: some View {
         Text("SplashScreenView")
+            .onAppear() {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                    router.push(to: .mainTab)
+                }
+            }
     }
 }
 
