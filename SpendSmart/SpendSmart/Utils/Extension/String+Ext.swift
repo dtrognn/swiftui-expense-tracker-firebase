2c3cb2f652324b32e909b04b6893330074c8b982
dtrognn <dtrognn@gmail.com>
2024-04-03 16:58:32 +0700

    String+Ext

---
+++ SpendSmart/SpendSmart/Utils/Extension/String+Ext.swift
@@ -0,0 +1,14 @@
+//
+//  String+Ext.swift
+//  SpendSmart
+//
+//  Created by dtrognn on 03/04/2024.
+//
+
+import SwiftUI
+
+extension String {
+    func toLocalizedStringKey() -> LocalizedStringKey {
+        return LocalizedStringKey(self)
+    }
+}
