670db4173dd9add98b911083287f667fc35bef1e
dtrognn <dtrognn@gmail.com>
2024-04-09 08:29:35 +0700

    add TransitionManager

---
+++ SpendSmart/SpendSmart/Features/Mânger/ TransactionManager.swift
@@ -0,0 +1,86 @@
+//
+//   TransactionManager.swift
+//  SpendSmart
+//
+//  Created by dtrognn on 08/04/2024.
+//
+
+import Foundation
+
+class TransactionManager: BaseViewModel {
+    static let shared = TransactionManager()
+
+    private var authService = AuthServiceManager.shared
+
+    func getTransactionList(type: TransactionType, completion: @escaping (Result<[Transaction], Error>) -> Void) {
+        apiGetTransactionList(type: type.rawValue, completion: completion)
+    }
+
+    func addTransaction(_ newTransaction: Transaction, completion: @escaping (Result<Void, Error>) -> Void) {
+        apiAddTransaction(newTransaction, completion: completion)
+    }
+
+    func updateTransaction(_ transaction: Transaction, completion: @escaping (Result<Void, Error>) -> Void) {
+        apiUpdateTransaction(transaction, completion: completion)
+    }
+
+    func deleteTransaction(_ transactionID: String, completion: @escaping (Result<Void, Error>) -> Void) {
+        apiDeleteTransaction(transactionID, completion: completion)
+    }
+
+    private func apiGetTransactionList(type: String, completion: @escaping (Result<[Transaction], Error>) -> Void) {
+        guard let uid = authService.userSesstion?.uid else { return }
+
+        FIRTransactionsCollection
+            .whereField("uid", isEqualTo: uid)
+            .whereField("type", isEqualTo: type)
+            .getDocuments { [weak self] snapshot, error in
+                if let error = error {
+                    completion(.failure(error))
+                }
+
+                guard let snapshot = snapshot else {
+                    completion(.failure(NSError(domain: "Snapshot is nil", code: -1, userInfo: nil)))
+                    return
+                }
+
+                let transactions = snapshot.documents.compactMap { document in
+                    try? document.data(as: Transaction.self)
+                }
+                completion(.success(transactions))
+            }
+    }
+
+    private func apiAddTransaction(_ newTransaction: Transaction, completion: @escaping (Result<Void, Error>) -> Void) {
+        try? FIRTransactionsCollection.document(newTransaction.id)
+            .setData(newTransaction.asDictionary()) { [weak self] error in
+                if let error = error {
+                    completion(.failure(error))
+                } else {
+                    completion(.success(()))
+                }
+            }
+    }
+
+    private func apiUpdateTransaction(_ transaction: Transaction, completion: @escaping (Result<Void, Error>) -> Void) {
+        try? FIRTransactionsCollection.document(transaction.id)
+            .updateData(transaction.asDictionary(), completion: { [weak self] error in
+                if let error = error {
+                    completion(.failure(error))
+                } else {
+                    completion(.success(()))
+                }
+            })
+    }
+
+    private func apiDeleteTransaction(_ transactionID: String, completion: @escaping (Result<Void, Error>) -> Void) {
+        FIRTransactionsCollection.document(transactionID)
+            .delete { [weak self] error in
+                if let error = error {
+                    completion(.failure(error))
+                } else {
+                    completion(.success(()))
+                }
+            }
+    }
+}
